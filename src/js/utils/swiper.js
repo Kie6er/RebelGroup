82777c7a0ccc77e4b427d1068043c73035fa96a8
NastiaCandor <andubova98@gmail.com>
2023-12-25 18:49:46 +0300

    fix: change slider on barbershop page

--- src/js/utils/swiper.js
+++ src/js/utils/swiper.js
@@ -98,7 +98,42 @@ $(document).ready(() => {
 			},
 		});
 	}
-	if ($('.barbershop-advantages__slider').length) {
+	// if ($('.barbershop-advantages__slider').length) {
+	// 	let barbershopAdvantagesSlider = new Swiper('.barbershop-advantages__slider', {
+	// 		modules: [Navigation, Pagination, EffectCreative],
+	// 		slidesPerView: 'auto',
+	// 		spaceBetween: `${remToPx(2)}rem`,
+	// 		direction: 'horizontal',
+	// 		autoHeight: true,
+	// 		speed: 1200,
+	// 		navigation: {
+	// 			prevEl: '.barbershop-advantages__slider-navigation-btn--prev',
+	// 			nextEl: '.barbershop-advantages__slider-navigation-btn--next',
+	// 		},
+	// 		pagination: {
+	// 			type: 'fraction',
+	// 			el: '.barbershop-advantages__slider-fraction',
+	// 		},
+	// 		effect: window.outerWidth > 768 ? 'creative' : 'slide',
+	// 		creativeEffect: {
+	// 			limitProgress: 2,
+	// 			next: {
+	// 				translate: ['39.6rem', '20.1rem', 0],
+	// 			},
+	// 			prev: {
+	// 				translate: ['-39.5rem', '33.5rem', 0],
+	// 			},
+	// 		},
+	// 		breakpoints: {
+	// 			768: {
+	// 				slidesPerView: 2,
+	// 				spaceBetween: `${remToPx(2.4)}rem`,
+	// 			},
+	// 		},
+	// 	});
+	// }
+  var interleaveOffset = 0.5;
+  if ($('.barbershop-advantages__slider').length) {
 		let barbershopAdvantagesSlider = new Swiper('.barbershop-advantages__slider', {
 			modules: [Navigation, Pagination, EffectCreative],
 			slidesPerView: 'auto',
@@ -106,6 +141,7 @@ $(document).ready(() => {
 			direction: 'horizontal',
 			autoHeight: true,
 			speed: 1200,
+      allowTouchMove: true,
 			navigation: {
 				prevEl: '.barbershop-advantages__slider-navigation-btn--prev',
 				nextEl: '.barbershop-advantages__slider-navigation-btn--next',
@@ -126,6 +162,7 @@ $(document).ready(() => {
 			},
 			breakpoints: {
 				768: {
+          allowTouchMove: false,
 					slidesPerView: 2,
 					spaceBetween: `${remToPx(2.4)}rem`,
 				},
